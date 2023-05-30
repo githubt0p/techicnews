49c9bf296e3b537d954c120c7176593d1867f6ab
githubt0p <nikm0311@gmail.com>
2023-05-30 21:29:28 +0400

    make app, 2 HTML files and some fixes in code

---
+++ technicnews/technews/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TechnewsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'technews'
---
+++ technicnews/technews/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('new_post/', views.creating)
+    ]
